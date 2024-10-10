fb0b647ee4461cbe78f718cf5548b3aee2338b30
Ahmad Bani Yaseen <ahmadbaniyassen8@gmail.com>
2024-10-10 10:51:57 +0300

    Create Service

---
+++ BusinessCardAPI/BusinessCard.Infra/Service/BusinessCardService.cs
@@ -0,0 +1,50 @@
+﻿using BusinessCard.Core.Repository;
+using BusinessCard.Core.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessCard.Infra.Service
+{
+    public class BusinessCardService : IBusinessCardService
+    {
+        private readonly IBusinessCardRepository _businessCardRepository;
+
+        public BusinessCardService(IBusinessCardRepository businessCardRepository)
+        {
+            _businessCardRepository = businessCardRepository;
+        }
+
+        public void CreateBusinessCard(Core.Data.BusinessCard businessCard)
+        {
+            _businessCardRepository.CreateBusinessCard(businessCard);
+        }
+
+        public void DeleteBusinessCard(int id)
+        {
+            _businessCardRepository.DeleteBusinessCard(id);
+        }
+
+        public List<Core.Data.BusinessCard> GetAllBusinessCard()
+        {
+           return  _businessCardRepository.GetAllBusinessCard();
+        }
+
+        public Core.Data.BusinessCard GetByBusinessCardId(int id)
+        {
+         return  _businessCardRepository.GetByBusinessCardId(id);
+        }
+
+        public Core.Data.BusinessCard GetFilterBusinessCard(string name, DateOnly DateOfBirth, string Phone, string Gender, string Email)
+        {
+            return _businessCardRepository.GetFilterBusinessCard(name,DateOfBirth,Phone,Gender,Email);
+        }
+
+        public void UpdateBusinessCard(Core.Data.BusinessCard businessCard)
+        {
+           _businessCardRepository.UpdateBusinessCard(businessCard);
+        }
+    }
+}
